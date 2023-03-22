e843b333b119ef1d0136f1cadf3eb6f876984085
Gage Harmon <gaharmon1@gmail.com>
2023-03-22 17:06:49 -0600

    added weapons and attack animation with weapon sprite

--- level.py
+++ level.py
@@ -4,13 +4,21 @@ from tile import Tile
 from player import Player
 from support import *
 from random import choice
+from weapon import Weapon
 
 class Level:
     def __init__(self):
         
+        #get display surface
         self.display_surface = pygame.display.get_surface()
+        
+        #sprite group setup
         self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
+        
+        #attack sprites
+        self.current_attack = None
+        
         #sprite setup
         self.create_map()
     
@@ -40,8 +48,16 @@ class Level:
                             surf = graphics['objects'][int(col)]
                             Tile((x,y), [self.visible_sprites, self.obstacle_sprites], 'object', surf)
         
-        self.player = Player ((2000,1430),[self.visible_sprites],self.obstacle_sprites)
-        
+        self.player = Player ((2000,1430),[self.visible_sprites], self.obstacle_sprites, self.create_attack, self.destroy_attack)
+    
+    def create_attack(self):
+        self.current_attack = Weapon(self.player, [self.visible_sprites])
+    
+    def destroy_attack(self):
+        if self.current_attack:
+            self.current_attack.kill()
+        self.current_attack = None
+    
     def run(self):
         #update and draw the game
         self.visible_sprites.custom_draw(self.player)
