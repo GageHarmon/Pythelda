b4184b8c1956eab99b26fbe64f923c5b9bf6ec3a
Gage Harmon <gaharmon1@gmail.com>
2023-03-22 21:39:33 -0600

    added magic spells on cast, and swap spells 'e', added list of spells in settings.py

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ from settings import *
 from support import import_folder
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups, obstacle_sprites, create_attack, destroy_attack):
+    def __init__(self, pos, groups, obstacle_sprites, create_attack, destroy_attack, create_magic):
         super().__init__(groups)
         self.image = pygame.image.load('./sprites/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
@@ -32,8 +32,11 @@ class Player(pygame.sprite.Sprite):
         self.switch_duration_cooldown = 200
         
         #magic
+        self.create_magic = create_magic
         self.magic_index = 0
         self.magic = list(magic_data.keys())[self.magic_index]
+        self.can_switch_magic = True
+        self.magic_switch_time = None
         
         #stats
         self.stats = {'health': 100, 'energy': 60, 'attack': 10, 'magic': 4, 'speed': 5}
@@ -85,6 +88,10 @@ class Player(pygame.sprite.Sprite):
             if keys[pygame.K_LCTRL]:
                 self.attacking = True
                 self.attack_time = pygame.time.get_ticks()
+                style = list(magic_data.keys())[self.magic_index]
+                strength = list(magic_data.values())[self.magic_index]['strength'] + self.stats['magic']
+                cost = list(magic_data.values())[self.magic_index]['cost']
+                self.create_magic(style, strength, cost)
             
             #weapon switch 'q'   
             if keys[pygame.K_q] and self.can_switch_weapon:
@@ -97,6 +104,18 @@ class Player(pygame.sprite.Sprite):
                     self.weapon_index = 0
                     
                 self.weapon = list(weapon_data.keys())[self.weapon_index]
+                
+            #magic switch
+            if keys[pygame.K_e] and self.can_switch_magic:
+                self.can_switch_weapon = False
+                self.magic_switch_time = pygame.time.get_ticks()
+                
+                if self.magic_index < len(list(magic_data.keys())) - 1:
+                    self.magic_index += 1
+                else:
+                    self.magic_index = 0
+                    
+                self.magic = list(magic_data.keys())[self.magic_index]
                   
     def get_status(self):
         
@@ -104,7 +123,7 @@ class Player(pygame.sprite.Sprite):
         if self.direction.x == 0 and self.direction.y == 0:
             if not 'idle' in self.status and not 'attack' in self.status:
                 self.status = self.status + '_idle'
-                
+
         if self.attacking:
             self.direction.x = 0
             self.direction.y = 0
